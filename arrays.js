05b3c70775a937b0c20fd70824e7c41dc1e5718c
Kathy Wang <kathy.x.wang@gmail.com>
2018-08-19 00:22:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0);
+  array.slice(1);
   return array;
 }
 
